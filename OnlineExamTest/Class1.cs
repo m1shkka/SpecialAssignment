8870ec46b4cf0ad66d4610bf2c03fa49b3d0a295
m1shkka <mishamaxixi@gmail.com>
2018-08-10 18:19:00 +0300

    trial commit

--- OnlineExamTest/Class1.cs
+++ OnlineExamTest/Class1.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace OnlineExamTest
 {
+    [TestFixture]
     public class Class1
     {
+        [Test]
+        public void LoginTest()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://51.144.34.125/");
+
+            IWebElement element = driver.FindElement(By.XPath("//*[@id=\"gn - menu\"]/li[4]/a"));
+            element.Click();
+
+            Assert.AreEqual("http://51.144.34.125/Account/Login", driver.Url);
+
+        } 
     }
 }
